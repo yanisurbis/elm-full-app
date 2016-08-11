58310af191e1499797056f6446c2741d60692120
Yanis <yanis.urbis@gmail.com>
2016-08-11 21:43:32 +0300

    Made small modules, define players, define players list

---
+++ src/Main.elm
@@ -0,0 +1,31 @@
+module Main exposing (..)
+
+import Html.App
+import Messages exposing (Msg)
+import Models exposing (Model)
+import View exposing (view)
+import Update exposing (update)
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
