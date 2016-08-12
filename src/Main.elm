24daf26ea3d7234f3a6e8b9ae18c58bbed5aee2c
Yanis <yanis.urbis@gmail.com>
2016-08-12 16:49:08 +0300

    Main

--- src/Main.elm
+++ src/Main.elm
@@ -2,30 +2,24 @@ module Main exposing (..)
 
 import Html.App
 import Messages exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
 
-
-init : ( Model, Cmd Msg )
+init : (Model, Cmd Msg)
 init =
-    ( "Hello", Cmd.none )
-
+  (initialModel , Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
-
-
+  Sub.none
 
 -- MAIN
 
-
-main : Program Never
 main =
-    Html.App.program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+  Html.App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
